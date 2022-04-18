172b8eb62a6124d7db6cad0d0082fb5b9f989cca
Taslim A Hussain <taslim@outlook.co.nz>
2022-04-18 13:33:36 +1200

    Folder structure

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglifycss = require('gulp-uglifycss');
+
+const build = () => {
+    return src(['src/sass/**/*.scss', '!src/sass/**/variables.scss'])
+        .pipe(sass())
+        .pipe(uglifycss())
+        .pipe(dest('dist'));
+};
+
+
+const watchTask = () => {
+    watch(['src/**/*.scss', '*.html'], build);
+};
+
+
+
+exports.default = series(build, watchTask);
